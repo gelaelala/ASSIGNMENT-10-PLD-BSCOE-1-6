48e08223a33c46f4fe75ecdbe31937acc7a78e39
unknown <angelaescariocorpuz@gmail.com>
2022-02-18 17:12:59 +0800

    added personal information for contact tracing

---
+++ QR Code Generator.py
@@ -0,0 +1,16 @@
+import pyqrcode 
+from pyqrcode import QRCode
+import png
+
+contact_tracing = """---CONTACT TRACING INFORMATION (as of February 18, 2022)---
+
+Personal Details:
+
+Name: Angela E. Corpuz
+Age: 19
+Birthday: December 14, 2002
+Address: Blk 47 Lot 6 Phase 2-E Mabuhay Homes, Brgy. Dila, Santa Rosa City, Laguna
+Email: angelaescariocorpuz@gmail.com
+Phone Number: 09451426999
+
+"""
