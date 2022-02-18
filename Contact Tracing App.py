8ef8e35c4483ceeed9d86015b2409b530f5c205b
unknown <angelaescariocorpuz@gmail.com>
2022-02-18 16:42:28 +0800

    minor revisions and added notes in some of the codes

--- Contact Tracing App.py
+++ Contact Tracing App.py
@@ -13,9 +13,9 @@
 #	- Create a demo of your program (1-2 min) and send it directly to my messenger.
 
 
-import cv2
-from datetime import datetime 
-import os as access 
+import cv2 #imported cv2 in order to access the webcam
+from datetime import datetime #imported datetime in order to get the date and time on when did the user scanned the qr code
+import os as access #imported access so that text file can be opened in its default program (Notepad)
 
 def QR_code ():
     webcam = cv2.VideoCapture (0)
@@ -23,25 +23,24 @@ def QR_code ():
     while True:
         _, img = webcam.read ()
         data, one, _ = qr_detector.detectAndDecode (img)
-        cv2.imshow ('QR Code Scanner', img)
+        cv2.imshow ('QR Code Scanner', img) #opens webcam in new window
         if data:
             data_to_textfile (data)
             break
-        if cv2.waitKey (1) == ord('q'):
+        if cv2.waitKey (1) == ord('q'): #pressing q from keyboard will close the webcam
+            cv2.destroyAllWindows
             break
     
 def data_to_textfile (data):
-        current_date_time = datetime.now ()
-        file = open ("Contact Tracing.txt", 'w+')
-        file.write (f'{data} \n')
+        current_date_time = datetime.now () #getting the date and time as to when the QR code was scanned
+        file = open ("Contact Tracing.txt", 'w+') #creates new file 
+        file.write (f'{data} \n') #data read in QR code will be written in the text file
+        file.write ('\n') #creates a new line (acts as the spacing between the last line from QR code and first line for the date and time part)
         file.write ('QR Code scanned in: \n')
-        file.write (f'     Date: {current_date_time.strftime ("%B %d, %Y")} \n')
-        file.write (f'     Time: {current_date_time.strftime ("%I:%M %p")}')
+        file.write (f'     Date: {current_date_time.strftime ("%B %d, %Y")} \n') #format of date will be in "month" "day", "year" (e.g. February 18, 2022)
+        file.write (f'     Time: {current_date_time.strftime ("%I:%M %p")}') #format of time will be in hours:minutes and is stated in 12-hour format instead of 24-hour format (e.g. 12:22 PM)
         file.close ()
-        access.startfile ("Contact Tracing.txt")
+        access.startfile ("Contact Tracing.txt") #after creating the file, text file will start/open in Notepad app
 
-def main ():
-    QR_code ()
-    cv2.destroyAllWindows
+QR_code () #this will execute the whole program
 
-main ()
